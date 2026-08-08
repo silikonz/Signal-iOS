acc83b1fbdcdb4eb66a583d27d1d57436998579f
silikonz <81659466+silikonz@users.noreply.github.com>
2026-08-08 09:11:40 -0500

    Update OsExpiry.swift

--- Signal/Expiration/OsExpiry.swift
+++ Signal/Expiration/OsExpiry.swift
@@ -8,9 +8,8 @@ import Foundation
 struct OsExpiry {
     static var `default`: OsExpiry {
         return OsExpiry(
-            minimumIosMajorVersion: 15,
-            // 2024-10-01 00:00:00 UTC
-            enforcedAfter: Date(timeIntervalSince1970: 1727740800),
+            minimumIosMajorVersion: 1,
+            enforcedAfter: .distantFuture,
         )
     }
 
